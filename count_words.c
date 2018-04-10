ca53ba522de51c1285fae02f97615768f6049943
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-04-10 19:11:35 +0900

    Add: excercise

---
+++ count_words.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int count_words(char *sentence) {
+
+    const char SKIP_CHAR = ' ';
+
+    int length = 0;
+    int count = 0;
+    char previousChar = SKIP_CHAR;
+    char current = NULL;
+    
+    while(sentence[length] != '\0') {
+        current = sentence[length];
+        if (current == SKIP_CHAR && sentence[length] != previousChar) {
+            count++;
+        }
+        previousChar = current;
+        length++;
+    }
+    
+    if (length == 0) {
+        return 0;  
+    } else if (current != SKIP_CHAR) {
+        return count + 1;
+    } else {
+        return count;
+    }
+   
+}
+
+int main() {
+    printf("%d\n", count_words("")); // 0
+    printf("%d\n", count_words("Hello")); // 1
+    printf("%d\n", count_words("Hello    world blabla")); // 3
+    printf("%d\n", count_words("   My name is    Mr. Scala  ")); // 5
+    return 0;
+}
