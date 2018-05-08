6cb5a0baaa207fac2866f9dabe31cb30172ef87b
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-05-08 10:49:15 +0900

    Fix: minus pattern

--- my_getnbr.c
+++ my_getnbr.c
@@ -9,6 +9,17 @@ int my_strlen(const char *str) {
   return size;
 }
 
+// 2^3 = 2 * 2 * 2
+int my_power_rec(int x, int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return x * my_power_rec(x, n - 1);
+    }
+}
+
+// 12345678 = 1 * 10^7 + 2 * 10^6 + 3 * 10^5 ...
+
 // string base -> integer 
 // 101011 "01" -> 43
 // AFE "0123456789ABCDEF"
@@ -18,14 +29,29 @@ int my_strlen(const char *str) {
 // ----123 "012345679" -> 123
 int my_getnbr(char *str, char *base) {
   int baseLength = my_strlen(base);
-  int inputLength = my_strlen(str);
+  int length = my_strlen(str) - 1;
   int result = 0;
-
-  for(int i = 0; i < inputLength; i++) {
-    for(int j = 0; j < baseLength; j++) {
-        if(str[i] == base[j])
+  int minusCount = 0;
+  
+  for (int i = 0; i < str[i]; i++) {
+    for (int baseIndex = 0; baseIndex < baseLength; baseIndex++) {
+      if (str[i] == base[baseIndex]) {
+        printf("%c * %d^%d\n", str[i], baseLength, length);
+        result += baseIndex * my_power_rec(baseLength, length);
+        length--;
+        break;
+      } else if (str[i] == '-') {
+        minusCount++;
+        length--;
+        break;
+      }
     }
   }
+  return result * my_power_rec(-1, minusCount);
+}
 
+int main() {
+  printf("%d\n", my_getnbr("--1111", "01")); // 15
+  printf("%d\n", my_getnbr("---1111", "01")); // -15
   return 0;
 }
